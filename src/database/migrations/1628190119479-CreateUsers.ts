5ccd2b6461e0b1d20737bb4746fbbb42f6d9f043
Kochiyama <marcelokochiyama2210@gmail.com>
2021-08-05 15:22:15 -0400

    feat: create user table

---
+++ src/database/migrations/1628190119479-CreateUsers.ts
@@ -0,0 +1,51 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm'
+
+export class CreateUsers1628190119479 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'users',
+        columns: [
+          {
+            name: 'id',
+            type: 'UUID',
+            isPrimary: true,
+            generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
+          },
+          {
+            name: 'name',
+            type: 'TEXT',
+          },
+          {
+            name: 'email',
+            type: 'TEXT',
+            isUnique: true,
+          },
+          {
+            name: 'password',
+            type: 'TEXT',
+          },
+          {
+            name: 'active',
+            type: 'BOOLEAN',
+          },
+          {
+            name: 'created_at',
+            type: 'TIMESTAMPTZ',
+            default: 'now()',
+          },
+          {
+            name: 'updated_at',
+            type: 'TIMESTAMPTZ',
+            default: 'now()',
+          },
+        ],
+      })
+    )
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    queryRunner.dropTable('users')
+  }
+}
