58456c030782126a9354fe898c6b325c9fa4ccca
Kochiyama <marcelokochiyama2210@gmail.com>
2021-08-09 13:26:57 -0400

    feat: add user already exists verification

--- src/services/CreateUserService.ts
+++ src/services/CreateUserService.ts
@@ -2,6 +2,7 @@ import IUserRepository from '../repositories/IUserRepository';
 import UserRepository from '../repositories/UserRepository';
 import { hash } from 'bcrypt';
 import User from '../models/User';
+import AppError from '../errors/AppError';
 
 interface Request {
   name: string;
@@ -18,6 +19,15 @@ class CreateUserService {
 
   public async execute({ name, email, password }: Request): Promise<User> {
     const passwordHash = await hash(password, 8);
+
+    const alreadyExists = await this.userRepository.findByEmail(email);
+
+    if (alreadyExists) {
+      throw new AppError(
+        'Usuario já existe, tente logar com suas credenciais',
+        401,
+      );
+    }
 
     const user = await this.userRepository.create({
       name,
