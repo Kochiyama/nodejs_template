5589a6bebd885f9ffbca776c9c7cfa1a956cbff6
Kochiyama <marcelokochiyama2210@gmail.com>
2021-08-09 13:24:33 -0400

    fix: add missing async-error import

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,8 @@
 import 'reflect-metadata';
 import './config/env';
-import express, { NextFunction, Request, Response } from 'express';
 import './database';
+import 'express-async-errors';
+import express, { NextFunction, Request, Response } from 'express';
 import routes from './routes';
 import AppError from './errors/AppError';
 
@@ -10,16 +11,14 @@ const app = express();
 app.use(express.json());
 app.use(routes);
 
-app.use((err: Error, req: Request, res: Response, _: NextFunction) => {
+app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
   if (err instanceof AppError) {
-    return res
+    return response
       .status(err.statusCode)
       .json({ status: 'error', message: err.message });
   }
-
-  console.error(err);
-
-  return res
+  console.log(err);
+  return response
     .status(500)
     .json({ status: 'error', message: 'Internal server error' });
 });
