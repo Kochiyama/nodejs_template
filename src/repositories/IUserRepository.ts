17b3fda2952239cc5f4c0f348f381acbb6347e25
Kochiyama <marcelokochiyama2210@gmail.com>
2021-08-09 13:27:19 -0400

    fix: typo on return type

--- src/repositories/IUserRepository.ts
+++ src/repositories/IUserRepository.ts
@@ -2,6 +2,6 @@ import { CreateUserDTO } from '../dtos/CreateUserDTO';
 import User from '../models/User';
 
 export default interface IUserRepository {
-  findByEmail: (email: string) => Promise<User> | undefined;
+  findByEmail: (email: string) => Promise<User | undefined>;
   create: (createUserDto: CreateUserDTO) => Promise<User>;
 }
