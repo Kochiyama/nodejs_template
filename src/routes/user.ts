e5be189c829b5993cff98b64504b954f2d22e9db
Kochiyama <marcelokochiyama2210@gmail.com>
2021-08-09 09:51:38 -0400

    feat: create user controller

---
+++ src/routes/user.ts
@@ -0,0 +1,10 @@
+import { Request, Response, Router } from 'express';
+import UserController from '../controllers/UserController';
+
+const userRoutes = Router();
+
+const userController = new UserController();
+
+userRoutes.post('/', userController.create);
+
+export default userRoutes;
