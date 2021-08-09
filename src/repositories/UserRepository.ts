faa52c46746d457a1945b822fbf38fd8b3ba2946
Kochiyama <marcelokochiyama2210@gmail.com>
2021-08-09 09:06:37 -0400

    feat: create user repo

---
+++ src/repositories/UserRepository.ts
@@ -0,0 +1,37 @@
+import { getRepository, Repository } from 'typeorm';
+import { CreateUserDTO } from '../dtos/CreateUserDTO';
+import User from '../models/User';
+import IUserRepository from './IUserRepository';
+
+class UserRepository implements IUserRepository {
+  private ormRepository: Repository<User>;
+
+  constructor() {
+    this.ormRepository = getRepository(User);
+  }
+  findByEmail: (email: string) => Promise<User> | undefined;
+
+  public async async(email: string) {
+    const user = await this.ormRepository.findOne({
+      where: {
+        email,
+      },
+    });
+
+    return user;
+  }
+
+  public async create({ name, email, password }: CreateUserDTO) {
+    const user = this.ormRepository.create({
+      name,
+      email,
+      password,
+    });
+
+    await this.ormRepository.save(user);
+
+    return user;
+  }
+}
+
+export default UserRepository;
