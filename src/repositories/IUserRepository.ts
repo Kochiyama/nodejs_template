60608b4477b5a1640dda90f4d9687d07a93fb9e8
Kochiyama <marcelokochiyama2210@gmail.com>
2021-08-09 13:59:26 -0400

    feat: add new methods
    find user by id
    update user

--- src/repositories/IUserRepository.ts
+++ src/repositories/IUserRepository.ts
@@ -2,6 +2,8 @@ import { CreateUserDTO } from '../dtos/CreateUserDTO';
 import User from '../models/User';
 
 export default interface IUserRepository {
+  findById: (id: string) => Promise<User | undefined>;
   findByEmail: (email: string) => Promise<User | undefined>;
   create: (createUserDto: CreateUserDTO) => Promise<User>;
+  save: (user: User) => Promise<User>;
 }
