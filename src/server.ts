4cf2d49f931a4547041d57c923e758a2aa2f4637
Kochiyama <marcelokochiyama2210@gmail.com>
2021-08-09 10:04:05 -0400

    feat: add async error handling

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,26 @@
 import 'reflect-metadata';
-import express, { Request, Response } from 'express';
+import express, { NextFunction, Request, Response } from 'express';
 import './database';
 import routes from './routes';
+import AppError from './errors/AppError';
 
 const app = express();
 
 app.use(express.json());
-
 app.use(routes);
+
+app.use((err: Error, req: Request, res: Response, _: NextFunction) => {
+  if (err instanceof AppError) {
+    return res
+      .status(err.statusCode)
+      .json({ status: 'error', message: err.message });
+  }
+
+  console.error(err);
+
+  return res
+    .status(500)
+    .json({ status: 'error', message: 'Internal server error' });
+});
 
 app.listen(3030);
